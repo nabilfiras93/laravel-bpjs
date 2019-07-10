aa7c15dbeee3825270193abe6182b4faacf585aa
PROLINK-IT <hideyatsuapp@gmail.com>
2019-07-10 17:44:28 +0700

    reformat bpjs service

--- src/BpjsService.php
+++ src/BpjsService.php
@@ -36,9 +36,30 @@ class BpjsService{
     private $signature;
 
     /**
+     * X-Authorization header value
+     * @var string
+     */
+    private $authorization;
+
+    /**
      * @var string
      */
     private $secret_key;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @var string
+     */
+    private $app_code;
 
     /**
      * @var string
@@ -50,11 +71,14 @@ class BpjsService{
      */
     private $service_name;
 
-    public function __construct($configurations)
+    public function __construct($configurations = [])
     {
         $this->clients = new Client([
             'verify' => false
         ]);
+
+        // merge configs
+        $configurations = config('bpjs') + $configurations;
 
         foreach ($configurations as $key => $val){
             if (property_exists($this, $key)) {
@@ -63,15 +87,16 @@ class BpjsService{
         }
 
         //set X-Timestamp, X-Signature, and finally the headers
-        $this->setTimestamp()->setSignature()->setHeaders();
+        $this->setTimestamp()->setSignature()->setAuthorization()->setHeaders();
     }
 
     protected function setHeaders()
     {
         $this->headers = [
-            'X-cons-id' => $this->cons_id,
-            'X-Timestamp' => $this->timestamp,
-            'X-Signature' => $this->signature
+            'X-cons-id'       => $this->cons_id,
+            'X-Timestamp'     => $this->timestamp,
+            'X-Signature'     => $this->signature,
+            'X-Authorization' => $this->authorization,
         ];
         return $this;
     }
@@ -85,10 +110,18 @@ class BpjsService{
 
     protected function setSignature()
     {
-        $data = $this->cons_id . '&' . $this->timestamp;
+        $data = "{$this->cons_id}&{$this->timestamp}";
         $signature = hash_hmac('sha256', $data, $this->secret_key, true);
         $encodedSignature = base64_encode($signature);
         $this->signature = $encodedSignature;
+        return $this;
+    }
+
+    protected function setAuthorization()
+    {
+        $data = "{$this->username}:{$this->password}:{$this->app_code}";
+        $encodedAuth = base64_encode($data);
+        $this->authorization = "Basic {$encodedAuth}";
         return $this;
     }
 
@@ -98,7 +131,7 @@ class BpjsService{
         try {
             $response = $this->clients->request(
                 'GET',
-                $this->base_url . '/' . $this->service_name . '/' . $feature,
+                "{$this->base_url}/{$this->service_name}/{$feature}",
                 [
                     'headers' => $this->headers
                 ]
@@ -118,10 +151,10 @@ class BpjsService{
         try {
             $response = $this->clients->request(
                 'POST',
-                $this->base_url . '/' . $this->service_name . '/' . $feature,
+                "{$this->base_url}/{$this->service_name}/{$feature}",
                 [
                     'headers' => $this->headers,
-                    'json' => $data,
+                    'json'    => $data,
                 ]
             )->getBody()->getContents();
         } catch (\Exception $e) {
@@ -136,10 +169,10 @@ class BpjsService{
         try {
             $response = $this->clients->request(
                 'PUT',
-                $this->base_url . '/' . $this->service_name . '/' . $feature,
+                "{$this->base_url}/{$this->service_name}/{$feature}",
                 [
                     'headers' => $this->headers,
-                    'json' => $data,
+                    'json'    => $data,
                 ]
             )->getBody()->getContents();
         } catch (\Exception $e) {
@@ -155,10 +188,10 @@ class BpjsService{
         try {
             $response = $this->clients->request(
                 'DELETE',
-                $this->base_url . '/' . $this->service_name . '/' . $feature,
+                "{$this->base_url}/{$this->service_name}/{$feature}",
                 [
                     'headers' => $this->headers,
-                    'json' => $data,
+                    'json'    => $data,
                 ]
             )->getBody()->getContents();
         } catch (\Exception $e) {
