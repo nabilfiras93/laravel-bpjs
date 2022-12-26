eec299b0a18fdc6da2350aced781e640019f3ba7
Firman Hidayat <hideyatsuapp@gmail.com>
2022-12-26 21:54:17 +0700

    add getter for poli and provider and status pulang

--- src/PCare/Poli.php
+++ src/PCare/Poli.php
@@ -12,4 +12,21 @@ class Poli extends BasePcare
         $this->feature .= "/fktp";
         return $this;
     }
+
+    /**
+     * Get pcare poli fktp.
+     *
+     * @param int $offset
+     * @param int $limit
+     * @return array
+     */
+    public function getPoli($offset = null, $limit = null)
+    {
+        $this->setOffset($offset);
+        $this->setLimit($limit);
+
+        $this->setResponse($this->fktp()->index($this->offset, $this->limit));
+
+        return $this->response;
+    }
 }
