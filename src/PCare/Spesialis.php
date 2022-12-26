304e2e1a640beadc862d447f9fa5c7cb53bbea39
Firman Hidayat <hideyatsuapp@gmail.com>
2022-12-26 23:39:02 +0700

    add getter for spesialis group

--- src/PCare/Spesialis.php
+++ src/PCare/Spesialis.php
@@ -6,6 +6,10 @@ class Spesialis extends BasePcare
     protected $subFeature;
     protected $param;
     protected $isRujuk = false;
+    private $kodeSarana;
+    private $tanggalRujuk;
+    private $kodeKhusus;
+    private $nomorKartu;
 
     public function rujuk()
     {
@@ -61,5 +65,193 @@ class Spesialis extends BasePcare
     public function setRujuk(bool $isRujuk)
     {
         $this->isRujuk = $isRujuk;
+    }
+
+    public function setKodeSubspesialis($kodeSubspesialis)
+    {
+        $this->setKeyword($kodeSubspesialis);
+    }
+
+    public function setKodeSarana($kodeSarana = null)
+    {
+        if (!is_null($kodeSarana)) {
+            $this->kodeSarana = $kodeSarana;
+        }
+    }
+
+    public function setTanggalRujuk($tanggalRujuk = null)
+    {
+        if (!is_null($tanggalRujuk)) {
+            $this->tanggalRujuk = $tanggalRujuk;
+        }
+    }
+
+    /**
+     * Set kode khusus.
+     * 
+     * IGD = ALIH RAWAT
+     * HDL = HEMODIALISA
+     * JIW = JIWA
+     * KLT = KUSTA
+     * PAR = TB-MDR
+     * KEM = SARANA KEMOTERAPI
+     * RAT = SARANA RADIOTERAPI
+     * HIV = HIV-ODHA
+     * THA = THALASEMIA
+     * HEM = HEMOFILI
+     *
+     * @param string $kodeKhusus
+     * @return void
+     */
+    public function setKodeKhusus($kodeKhusus = null)
+    {
+        if (!is_null($kodeKhusus)) {
+            $this->kodeKhusus = $kodeKhusus;
+        }
+    }
+
+    public function setNomorKartu($nomorKartu = null)
+    {
+        if (!is_null($nomorKartu)) {
+            $this->nomorKartu = $nomorKartu;
+        }
+    }
+
+    /**
+     * Get pcare spesialis.
+     *
+     * @return array
+     */
+    public function getSpesialis()
+    {
+        $this->setResponse($this->index());
+
+        return $this->response;
+    }
+
+    /**
+     * Get pcare sub spesialis.
+     *
+     * @param string $kodeSubspesialis
+     * @return array
+     */
+    public function getSubSpesialis($kodeSubspesialis = null)
+    {
+        $this->setKodeSubspesialis($kodeSubspesialis);
+
+        $this->setResponse($this->subSpesialis($this->keyword)->index());
+
+        return $this->response;
+    }
+
+    /**
+     * Get pcare sarana.
+     *
+     * @param string $kodeSarana
+     * @return array
+     */
+    public function getSarana($kodeSarana = null)
+    {
+        $this->setKodeSarana($kodeSarana);
+
+        $this->setResponse($this->sarana()->index());
+
+        return $this->response;
+    }
+
+    /**
+     * Get pcare khusus.
+     *
+     * @return array
+     */
+    public function getKhusus()
+    {
+        $this->setResponse($this->khusus()->index());
+
+        return $this->response;
+    }
+
+    /**
+     * Get pcare faskes rujukan subspesialis.
+     *
+     * @param string $kodeSubspesialis
+     * @param string $kodeSarana
+     * @param string $tanggalRujuk
+     * @return array
+     */
+    public function getFaskesRujukanSubspesialis($kodeSubspesialis = null, $kodeSarana = null, $tanggalRujuk = null)
+    {
+        $this->setRujuk(true);
+        $this->setKodeSubspesialis($kodeSubspesialis);
+        $this->setKodeSarana($kodeSarana);
+        $this->setTanggalRujuk($tanggalRujuk);
+
+        $this->setResponse(
+            $this
+                ->rujuk()
+                ->subSpesialis($this->keyword)
+                ->sarana($this->kodeSarana)
+                ->tanggalRujuk($this->tanggalRujuk)
+                ->index()
+        );
+
+        return $this->response;
+    }
+
+    /**
+     * Get pcare faskes rujukan khusus.
+     *
+     * @param string $kodeKhusus
+     * @param string $nomorKartu
+     * @param string $tanggalRujuk
+     * @return array
+     */
+    public function getFaskesRujukanKhusus($kodeKhusus = null, $nomorKartu = null, $tanggalRujuk = null)
+    {
+        $this->setRujuk(true);
+        $this->setKodeKhusus($kodeKhusus);
+        $this->setNomorKartu($nomorKartu);
+        $this->setTanggalRujuk($tanggalRujuk);
+
+        $this->setResponse(
+            $this
+                ->rujuk()
+                ->khusus($this->kodeKhusus)
+                ->nomorKartu($this->nomorKartu)
+                ->tanggalRujuk($this->tanggalRujuk)
+                ->index()
+        );
+
+        return $this->response;
+    }
+
+    /**
+     * Get pcare faskes rujukan khusus.
+     *
+     * @param string $kodeKhusus
+     * @param string $kodeSubspesialis
+     * @param string $nomorKartu
+     * @param string $tanggalRujuk
+     * @return array
+     */
+    public function getFaskesRujukanKhususSubspesialis($kodeKhusus = null, $kodeSubspesialis = null, $nomorKartu = null, $tanggalRujuk = null)
+    {
+        $this->setRujuk(true);
+        $this->setKodeKhusus($kodeKhusus);
+        $this->setKodeSubspesialis($kodeSubspesialis);
+        $this->setNomorKartu($nomorKartu);
+        $this->setTanggalRujuk($tanggalRujuk);
+
+        $this->setResponse(
+            $this
+                ->rujuk()
+                ->khusus($this->kodeKhusus)
+                ->subSpesialis($this->keyword)
+                ->nomorKartu($this->nomorKartu)
+                ->tanggalRujuk($this->tanggalRujuk)
+                ->index()
+        );
+
+        return $this->response;
     }
 }
